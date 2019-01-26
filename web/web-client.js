3ee4dd284a7c6982813447de48917b5522724411
Sam Liem <samuelliem@gmail.com>
2019-01-26 13:41:43 +0000

    remove typo

--- web/web-client.js
+++ web/web-client.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
-});GIT
+});
 
 http.listen(port, function(){
     console.log('listening on *:' + port);
