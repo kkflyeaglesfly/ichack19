d6ee3ef7f87c82156d6a93f4595a249fceef90d9
dhrudevalia <dhrudevalia@gmail.com>
2019-01-27 03:42:19 +0000

    reintroduced reporting functions

--- cogni/src/main/kotlin/cogni/cogni/db/Posts.kt
+++ cogni/src/main/kotlin/cogni/cogni/db/Posts.kt
@@ -93,7 +93,8 @@ object Posts {
 
 
     fun removePost(postId : Long){
-        posts.remove(getPostById(postId))
+        //mark post as removed
+        //posts.remove(getPostById(postId))
     }
 
     fun upvotePost(postId: Long, userId : Long){
@@ -107,8 +108,8 @@ object Posts {
         //change karma to reflect decision
     }
 
-    fun upvoteReply(replyId: Long, userId: Long){
-        var reply : Reply = getReplyFromReplyId(replyId)
+    fun upvoteReply(replyId: Long, userId: Long, post: Post){
+        var reply : Reply = getReplyById(post, replyId)!!
 
         if (reply.downvotes.contains(Users.getUserById(userId))){
             reply.downvotes.remove(Users.getUserById(userId))
@@ -124,12 +125,8 @@ object Posts {
 
     }
 
-    private fun getReplyFromReplyId(replyId: Long): Reply {
-        return posts[0].replies[0]
-    }
-
-    fun downVoteReply(replyId: Long, userId: Long){
-        var reply : Reply = getReplyFromReplyId(replyId)
+    fun downVoteReply(replyId: Long, userId: Long, post: Post){
+        var reply : Reply = getReplyById(post, replyId)!!
 
         if(reply.upvotes.contains(Users.getUserById(userId))){
             reply.upvotes.remove(Users.getUserById(userId))
